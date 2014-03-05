03aa8c1b011e3e6b0d74ea613d0408d164c17252
gskielian <gregory.kielian@gmail.com>
2014-03-05 03:49:17 -0800

    added comments and extensive text/formatting to the front page.

--- src/com/example/a0_android_hello_world/MainActivity.java
+++ src/com/example/a0_android_hello_world/MainActivity.java
@@ -9,6 +9,13 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//note it is best practice to keep your strings in values->strings
+
+		//Random Tip: Customers like birds.
+
+		//Remember, we are separating the appearance from what's under-the-hood
+	  	  //from the above, one way to coordinate:
+	  	  //1 team-mate forking on the layout, and one of the logic
 		setContentView(R.layout.activity_main);
 	}
 
